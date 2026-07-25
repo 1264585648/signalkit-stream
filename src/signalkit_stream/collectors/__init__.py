4eabb222bd30a230a521735ac008bce0101d0c5c
1264585648 <77035161+1264585648@users.noreply.github.com>
2026-07-25 22:55:34 +0800

    refactor: export collector SDK types

--- src/signalkit_stream/collectors/__init__.py
+++ src/signalkit_stream/collectors/__init__.py
@@ -1,6 +1,13 @@
-from signalkit_stream.collectors.base import Collector
+from signalkit_stream.collectors.base import Collector, HTTPCollector, RetryPolicy
 from signalkit_stream.collectors.github import GitHubCollector
 from signalkit_stream.collectors.hackernews import HackerNewsCollector
 from signalkit_stream.collectors.rss import RSSCollector
 
-__all__ = ["Collector", "GitHubCollector", "HackerNewsCollector", "RSSCollector"]
+__all__ = [
+    "Collector",
+    "GitHubCollector",
+    "HTTPCollector",
+    "HackerNewsCollector",
+    "RSSCollector",
+    "RetryPolicy",
+]

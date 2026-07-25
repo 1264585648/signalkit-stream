33eb80f616351ee3fbab3ba515d53a2970f53260
1264585648 <77035161+1264585648@users.noreply.github.com>
2026-07-25 23:41:10 +0800

    feat: export Reddit collector types

--- src/signalkit_stream/collectors/__init__.py
+++ src/signalkit_stream/collectors/__init__.py
@@ -1,6 +1,7 @@
 from signalkit_stream.collectors.base import Collector, HTTPCollector, RetryPolicy
 from signalkit_stream.collectors.github import GitHubCollector
 from signalkit_stream.collectors.hackernews import HackerNewsCollector
+from signalkit_stream.collectors.reddit import RedditCollector, RedditOAuth
 from signalkit_stream.collectors.rss import RSSCollector
 
 __all__ = [
@@ -9,5 +10,7 @@ __all__ = [
     "HTTPCollector",
     "HackerNewsCollector",
     "RSSCollector",
+    "RedditCollector",
+    "RedditOAuth",
     "RetryPolicy",
 ]
